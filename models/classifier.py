314c63b74079cf335211d57a8ecd33ce189e6e9e
Basavaraj Hampiholi <basavaraj.hampiholi@uni-ulm.de>
2022-03-29 11:42:42 +0200

    model code

--- models/classifier.py
+++ models/classifier.py
@@ -5,89 +5,37 @@ from torch.nn import functional as F
 import torch.optim as optim
 import numpy as np
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
-class CNN_NLP(nn.Module):
-    """An 1D Convulational Neural Network for Sentence Classification."""
-    def __init__(self,
-                 pretrained_embedding=None,
-                 freeze_embedding=False,
-                 vocab_size=None,
-                 embed_dim=300,
-                 filter_sizes=[3, 4, 5],
-                 num_filters=[100, 100, 100],
-                 num_classes=219,
-                 dropout=0.5):
+class Sentence_CNN(nn.Module):
+	def __init__(self, vocab_size, embed_size, num_classes):
+		super(Sentence_CNN, self).__init__()
 
+		self.embedding = nn.Embedding(num_embeddings=vocab_size,embedding_dim=embed_size, padding_idx=0, max_norm=5.0)
 
-        super(CNN_NLP, self).__init__()
-        # Embedding layer
-        if pretrained_embedding is not None:
-            self.vocab_size, self.embed_dim = pretrained_embedding.shape
-            self.embedding = nn.Embedding.from_pretrained(pretrained_embedding,
-                                                          freeze=freeze_embedding)
-        else:
-            self.embed_dim = embed_dim
-            self.embedding = nn.Embedding(num_embeddings=vocab_size,
-                                          embedding_dim=self.embed_dim,
-                                          padding_idx=0,
-                                          max_norm=5.0)
-        # Conv Network
-        self.conv1d_list = nn.ModuleList([
-            nn.Conv1d(in_channels=self.embed_dim,
-                      out_channels=num_filters[i],
-                      kernel_size=filter_sizes[i])
-            for i in range(len(filter_sizes))])
-        
-        # Fully-connected layer and Dropout
-        self.fc = nn.Linear(np.sum(num_filters), num_classes)
-        self.dropout = nn.Dropout(p=dropout)
+		self.conv1 = nn.Conv1d(in_channels=300,out_channels=128, kernel_size=3)#, groups=out_channels)
+		self.conv2 = nn.Conv1d(in_channels=300,out_channels=128, kernel_size=4)#, groups=out_channels)
+		self.conv3 = nn.Conv1d(in_channels=300,out_channels=128, kernel_size=5)#, groups=out_channels)
 
-    def forward(self, input_ids):
+		self.relu = nn.ReLU()
+
+		self.fc = nn.Linear(384, num_classes)
 
 
-        x_embed = self.embedding(input_ids).float()
-        
+	def forward(self, input_ids):
+		x = self.embedding(input_ids)
+		x_permute = x.permute(0, 2, 1)
 
-        x_reshaped = x_embed.permute(0, 2, 1)
-        x_conv_list = [F.relu(conv1d(x_reshaped)) for conv1d in self.conv1d_list]
+		conv1 = self.relu(self.conv1(x_permute))
+		conv2 = self.relu(self.conv2(x_permute))
+		conv3 = self.relu(self.conv3(x_permute))
 
+		max1 = F.max_pool1d(conv1, kernel_size=conv1.size(2))
+		max2 = F.max_pool1d(conv2, kernel_size=conv2.size(2))
+		max3 = F.max_pool1d(conv3, kernel_size=conv3.size(2))
 
-        x_pool_list = [F.max_pool1d(x_conv, kernel_size=x_conv.shape[2])
-            for x_conv in x_conv_list]
-        
-        x_fc = torch.cat([x_pool.squeeze(dim=2) for x_pool in x_pool_list],
-                         dim=1)
-        
-        # Compute logits. Output shape: (b, n_classes)
-        logits = self.fc(self.dropout(x_fc))
+		cat_features = torch.cat((max1, max2, max3), dim=1)
+		feats = torch.mean(cat_features, dim=2)
 
-        return logits
+		logits = self.fc(feats)
 
-def initilize_model(pretrained_embedding=None,
-                    freeze_embedding=False,
-                    vocab_size=None,
-                    embed_dim=300,
-                    filter_sizes=[3, 4, 5],
-                    num_filters=[100, 100, 100],
-                    num_classes=219,
-                    dropout=0.5,
-                    learning_rate=0.01):
-    """Instantiate a CNN model and an optimizer."""
-
-    assert (len(filter_sizes) == len(num_filters)), "filter_sizes and \
-    num_filters need to be of the same length."
-
-    # Instantiate CNN model
-    cnn_model = CNN_NLP(pretrained_embedding=pretrained_embedding,
-                        freeze_embedding=freeze_embedding,
-                        vocab_size=vocab_size,
-                        embed_dim=embed_dim,
-                        filter_sizes=filter_sizes,
-                        num_filters=num_filters,
-                        num_classes=219,
-                        dropout=0.5)
-
-    return cnn_model
+		return logits
