1d964e59c9732e25e23b044e4647c06810eb19af
Basavaraj Hampiholi <basavaraj.hampiholi@uni-ulm.de>
2022-03-29 14:36:49 +0200

    Update dsc_model.py

--- models/dsc_model.py
+++ models/dsc_model.py
@@ -10,36 +10,34 @@ class Sentence_CNN(nn.Module):
 	def __init__(self, vocab_size, embed_size, num_classes):
 		super(Sentence_CNN, self).__init__()
 
+		self.out_channels=64
 		self.embedding = nn.Embedding(num_embeddings=vocab_size,embedding_dim=embed_size, padding_idx=0, max_norm=5.0)
+		self.conv1 = nn.Conv1d(in_channels=embed_size, out_channels=self.out_channels, kernel_size=3, dilation=1, padding=1, groups=self.out_channels)
+		self.conv2 = nn.Conv1d(in_channels=embed_size, out_channels=self.out_channels, kernel_size=3, dilation=2, padding=2, groups=self.out_channels)
+		self.conv3 = nn.Conv1d(in_channels=embed_size, out_channels=self.out_channels, kernel_size=3, dilation=3, padding=3, groups=self.out_channels)
+		self.conv4 = nn.Conv1d(in_channels=embed_size, out_channels=self.out_channels, kernel_size=3, dilation=4, padding=4, groups=self.out_channels)
 
-		self.conv1 = nn.Conv1d(in_channels=300,out_channels=128, kernel_size=2)
-		self.conv2 = nn.Conv1d(in_channels=300,out_channels=128, kernel_size=3, padding=1)
-		self.conv3 = nn.Conv1d(in_channels=300,out_channels=128, kernel_size=4, padding=1)
-
-		self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+		self.conv1x1=nn.Conv1d(in_channels=256, out_channels=256, kernel_size=1)
 		self.relu = nn.ReLU()
 
-		self.fc = nn.Linear(384, num_classes)
-
+		self.fc = nn.Linear(256, num_classes)
 
 	def forward(self, input_ids):
+
 		x = self.embedding(input_ids)
 		x_permute = x.permute(0, 2, 1)
 
-		conv1 = self.pool(self.relu(self.conv1(x_permute)))
-		conv2 = self.pool(self.relu(self.conv2(x_permute)))
-		conv3 = self.pool(self.relu(self.conv3(x_permute)))
+		conv1 = self.conv1(x_permute)
+		conv2 = self.conv2(x_permute)
+		conv3 = self.conv3(x_permute)
+		conv4 = self.conv4(x_permute)
 
-		cat_features = torch.cat((conv1, conv2, conv3), dim=1)
-		feats = torch.mean(cat_features, dim=2)
+		cat_feats = torch.cat((conv1, conv2, conv3, conv4), dim=1)
+		conv_cat = self.relu(self.conv1x1(cat_feats))
+
+		max1 = F.max_pool1d(conv_cat, kernel_size=conv_cat.size(2))		
+		feats = torch.mean(max1, dim=2)
 
 		logits = self.fc(feats)
 
 		return logits
-
-
-
-
-
-
-
