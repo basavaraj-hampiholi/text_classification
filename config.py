660ec77629243fe0b54539c028c090519972ae83
Basavaraj Hampiholi <basavaraj.hampiholi@uni-ulm.de>
2022-03-30 12:00:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,12 @@ csv_train = 'csv_old/train_prepared.csv'
 csv_valid = 'csv_old/valid_prepared.csv'
 csv_test = 'csv_old/test_prepared.csv'
 
-np_train = 'npy/train_features.npy'
-np_valid = 'npy/valid_features.npy'
-np_test = 'npy/test_features.npy'
-np_trn_lbls = 'npy/train_labels.npy'
-np_val_lbls = 'npy/valid_labels.npy'
-np_tst_lbls = 'npy/test_labels.npy'
-
-#np_test = 'csv_old/test.npy'
+np_train = 'dataset/npy/train_features.npy'
+np_valid = 'dataset/npy/valid_features.npy'
+np_test = 'dataset/npy/test_features.npy'
+np_trn_lbls = 'dataset/npy/train_labels.npy'
+np_val_lbls = 'dataset/npy/valid_labels.npy'
+np_tst_lbls = 'dataset/npy/test_labels.npy'
 
 #path to the saved model
 ckpt_path = './save_model/dbpedia_chkpt.pt'
